4a4fb725d6ac0b22ded92e323041a6709ef9b67d
theonaun <theonaunheim@gmail.com>
2014-08-07 21:56:38 -0500

    Move docstring.

--- surgeo/__init__.py
+++ surgeo/__init__.py
@@ -1,15 +1,44 @@
+'''surgeo is a Bayesian Improved Surname Geocoding model written in Python 3.
+ ___ _   _ _ __ __ _  ___  ___  
+/ __| | | | '__/ _` |/ _ \/ _ \ 
+\__ \ |_| | | | (_| |  __/ (_) |
+|___/\__,_|_|  \__, |\___|\___/ 
+               |___/            
+               
+Python code by Theo Naunheim. Model created by Mark N. Elliot et al.
+
+############################################################################### 
+License
+###############################################################################
+
+The MIT License (MIT)
+
+Copyright (c) 2014 Theo Naunheim
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.'''
+
 import os
 import sys
 
 # Version check before imports. If not Python3, kill import
 if sys.hexversion < 0x03000000:
     raise ImportError('Python version of 3.0 or higher required.')
-
-# Kludgy fix for path   
-file_path = os.path.abspath(__file__)
-parent_dir = os.path.dirname(file_path)
-grandparent_dir = os.path.dirname(parent_dir)
-sys.path.append(grandparent_dir)
 
 # Put SurgeoModel, SurgeoResult, and SurgeoError in surgeo namespace
 from surgeo.utilities.class_file import SurgeoModel
