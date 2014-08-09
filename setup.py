5963274c8983bf7ef4df8779cd941a270c0ae475
theonaun <theonaunheim@gmail.com>
2014-08-08 19:07:49 -0500

    Fix setup.py

--- setup.py
+++ setup.py
@@ -5,13 +5,13 @@ from distutils.core import setup
 from distutils.core import DistutilsError
     
 setup(name='surgeo',
-      version='0.2.2000',
+      version='0.3.2000',
       description='Disparate impact testing through surname geocoding analysis',
       url = 'https://github.com/theonaun/surgeo',
       author='Theo Naunheim',
       author_email='theonaunheim@gmail.com',
       license='MIT',
-      packages=['surgeo.db', 'surgeo.model','surgeo.utilities'],
+      packages=['surgeo', 'surgeo.db', 'surgeo.model','surgeo.utilities'],
       classifiers=[
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Financial and Insurance Industry',
