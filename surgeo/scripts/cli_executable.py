a772445d13b971250b08180ac6d30aa8b239467e
theonaun <theonaunheim@gmail.com>
2014-11-15 23:05:51 -0600

    Divide up cli_executable into subroutines.

--- surgeo/scripts/cli_executable.py
+++ surgeo/scripts/cli_executable.py
@@ -1,7 +1,6 @@
 #!/usr/local/bin/python3
 #coding: utf-8
-'''This is an executable wrapper for surgeo.'''
-import inspect
+'''This is an executable wrapper for Surgeo.'''
 import sys
 
 import surgeo
@@ -43,21 +42,32 @@ def cli_main(*args):
 
     '''
 
-##### Parse arguments and setup
-    # default args = csv, model, pipe, setup, string, verbose
+    # Parse arguments and setup (csv, model, pipe, setup, string, verbose)
     parsed_args = surgeo.utilities.parser.get_parser_args()
     # setup folders and the like
     surgeo.setup_functions()
     # If quiet, no output. Adapter shunts all output.
     if parsed_args.quiet:
         surgeo.adapter.direct_to_null()
-##### Pipe
+    # Pipe argument
+    cli_pipe_subroutine(parsed_args)
+    # String argument
+    cli_string_subroutine(parsed_args)
+    # Setup argument
+    cli_setup_subroutine(parsed_args)
+    # Gui argument (no arguments)
+    cli_gui_subroutine(parsed_args)
+
+def cli_pipe_subroutine(parsed_args):
+    '''For the "Pipe" argument.'''
     if parsed_args.pipe:
         # Check what model
         if not parsed_args.model:
-            raise surgeo.SurgeoError('No model specified for analysis.')
+            raise surgeo.SurgeoError('No model specified.')
         # Instantiate model.
-        model_name = parsed_args.model
+        model_name = parsed_args.model[0]
+        module_name = parsed_args.model.lower().replace('model', '_model')
+        model_module = getattr(surgeo.models, module_name)
         model_class = getattr(surgeo.models, model_name)
         model = model_class()
         try:
@@ -76,46 +86,52 @@ def cli_main(*args):
                     surgeo.adapter.adaprint(result.as_string())
         except EOFError:
             pass
-##### String
-    elif parsed_args.string:
+
+def cli_string_subroutine(parsed_args):
+    '''"String" argument analyzes and returns a string.'''
+    if parsed_args.string:
         if not parsed_args.model:
-            raise surgeo.SurgeoError('No model specified for analysis.')
+            raise surgeo.SurgeoError('No model specified.')
         # Instantiate model.
-        model_name = parsed_args.model
+        model_name = parsed_args.model[0]
+        module_name = parsed_args.model.lower().replace('model', '_model')
+        model_module = getattr(surgeo.models, module_name)
         model_class = getattr(surgeo.models, model_name)
         model = model_class()
-                        stripped_line = line.strip()
-                        raw_arguments = stripped_line.split()
-                        argument_dict = {argument.split('=')[0]:
-                                         argument.split('=')[1]
-                                         for argument in raw_arguments}
-                        result = model.get_result(**argument_dict)
-                    except ValueError:
-                        result = surgeo.utilities.result.Result().errorify()
-                    surgeo.adapter.adaprint(result.as_string())
-##### Csv
-    elif parsed_args.file:
-        model = surgeo.SurgeoModel()
-        infile = parsed_args.file[0]
-        outfile = parsed_args.file[1]
-        model.process_csv(infile, outfile)
-##### Setup
-    elif parsed_args.complex:
-        model = surgeo.SurgeoModel()
-        zcta = parsed_args.complex[0]
-        surname = parsed_args.complex[1]
-        result = model.race_data(zcta, surname)
-        print(result.as_string)
-##### If no argument, GUI. Console should remain.
-    elif not any([parsed_args.setup,
-                  parsed_args.pipe,
-                  parsed_args.string,
-                  parsed_args.csv]):
+        # Convert arguments to dict
+        raw_arguments = parsed_args.string.split()
+        argument_dict = {argument.split('=')[0]:
+                         argument.split('=')[1]
+                         for argument in raw_arguments}
+        result = model.get_result(**argument_dict)
+        surgeo.adapter.adaprint(result.as_string())
+
+def cli_setup_subroutine(parsed_args):
+    '''"Setup" argument causes all databases to be created.'''
+    if parsed_args.setup:
+        if not parsed_args.model:
+            raise surgeo.SurgeoError('No model specified.')
+        # Instantiate model.
+        model_name = parsed_args.model[0]
+        module_name = parsed_args.model[0].lower().replace('model', '_model')
+        model_module = getattr(surgeo.models, module_name)
+        model_class = getattr(model_module, model_name)
+        model = model_class()
+        # If the model is bad
+        if not model.db_check():
+            model.db_create()
+
+def cli_gui_subroutine(parsed_args):
+    '''If no arguments, run the GUI.'''
+    if not any([parsed_args.setup,
+                parsed_args.pipe,
+                parsed_args.string,
+                parsed_args.csv]):
         try:
             from surgeo.scripts import gui_executable
             gui_executable()
         except ImportError:
-            raise SurgeoError('Cannot start GUI. Do you have tk support?')
+            raise surgeo.SurgeoError('Cannot start GUI. Do you have tk support?')
 
 if __name__ == "__main__":
     sys.exit(cli_main(sys.argv[1:]))
