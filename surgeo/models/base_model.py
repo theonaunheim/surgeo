e4dc51fede4ca6bffd0a2aa05c39c079dbe53694
Theo Naunheim <theonaunheim@gmail.com>
2019-12-11 19:13:16 -0600

    Add normalization routines to BaseModel.

--- surgeo/models/base_model.py
+++ surgeo/models/base_model.py
@@ -1,4 +1,5 @@
 import pathlib
+import string
 
 import pandas as pd
 
@@ -7,20 +8,48 @@ class BaseModel(object):
 
     def __init__(self):
         self._package_root = pathlib.Path(__file__).parents[1]
+        # Add geocode df
         self._GEOCODE_DF = pd.read_csv(
-            self._package_root /
-            'data' /
-            'geocode_2010.csv'
+            self._package_root / 'data' / 'population_2010.csv',
+            index_col='zcta5'
         )
+        # Convert geocode zip codes to strings
+        self._GEOCODE_DF.index = (
+            self._GEOCODE_DF.index
+                            .astype('str')
+                            .str
+                            .zfill(5)
+        )
+        # Add surname df
         self._SURNAME_DF = pd.read_csv(
-            self._package_root /
-            'data' /
-            'surname_2010.csv'
+            self._package_root / 'data' / 'surname_2010.csv',
+            index_col='name'
         )
 
-    def _normalize_name(self):
-        pass
+    def _normalize_names(self, names: pd.Series) -> pd.Series:
+        # Make a transalation table of unwanted characers
+        unwanted_characters = (
+            string.digits + 
+            string.punctuation + 
+            string.whitespace
+        )
+        translation_table =  str.maketrans('', '', unwanted_characters)
+        # Run our string operations
+        output = (
+            names.astype(str)
+                 .str.strip()
+                 .str.upper()
+                 .str.translate(translation_table)
+                 .str.replace('\sJ\.*R\.*\s*$', '')
+                 .str.replace('\sS\.*R\.*\s*$', '')
+                 .str.replace('\sIII\s*$',      '')
+                 .str.replace('\sIV\s*$',       '')
+        )
+        output.name = 'name'
+        return output
 
-    def _normalize_zcta(self):
-        pass
-
+    def _normalize_zctas(self, zcta: pd.Series) -> pd.Series:
+        converted = pd.Series(zcta.values, dtype=str)
+        zfilled = converted.str.zfill(5)
+        zfilled.name = 'zcta5'
+        return zfilled
--- surgeo/models/geocode_model.py
+++ surgeo/models/geocode_model.py
@@ -1,3 +1,5 @@
+import pandas as pd
+
 from .base_model import BaseModel
 
 
@@ -5,3 +7,14 @@ class GeocodeModel(BaseModel):
 
     def __init__(self):
         super().__init__()
+    
+    def get_probabilities(self, 
+                          zcta: pd.Series,
+                          missing='nan') -> pd.Series:
+        normalized_zctas = self._normalize_zctas(zcta)
+        result = (
+            self._GEOCODE_DF.map(
+                normalized_zctas,
+                left_index=True,
+            )
+        )
