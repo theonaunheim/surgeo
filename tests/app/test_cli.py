4c2269946acb1c04f3f0882235acbf891b995585
Theo Naunheim <theonaunheim@gmail.com>
2020-01-11 10:32:48 -0600

    Finish CLI testing routines.

--- tests/app/test_cli.py
+++ tests/app/test_cli.py
@@ -5,6 +5,7 @@ import sys
 import tempfile
 import unittest
 
+import numpy as np
 import pandas as pd
 
 import surgeo.app.cli
@@ -34,7 +35,7 @@ class TestSurgeoCLI(unittest.TestCase):
         if pathlib.Path(self._EXCEL_OUTPUT_PATH).exists():
             os.unlink(self._EXCEL_OUTPUT_PATH)
 
-    def compare(self, input_name, model_type, true_output_name):
+    def _compare(self, input_name, model_type, true_output_name):
         """"Helper function that runs the comparison"""
         # Generate input name based on input file
         input_path = str(self._DATA_FOLDER / input_name)
@@ -51,13 +52,17 @@ class TestSurgeoCLI(unittest.TestCase):
         # Read the true information
         df_true = pd.read_csv(self._DATA_FOLDER / true_output_name)
         # Compare values
-        self.assertTrue(
-            df_generated.equals(df_true)
-        )        
+        self._is_close_enough(df_generated, df_true)
+
+    def _is_close_enough(self, df_generated, df_true):
+        """Helper function to select floats, round them, and compare"""
+        df_generated = df_generated.select_dtypes(np.float64).round(4)
+        df_true = df_true.select_dtypes(np.float64).round(4)
+        self.assertTrue(df_generated.equals(df_true))          
 
     def test_surgeo_cli(self):
         """Test BISG model functionality of CLI"""
-        self.compare(
+        self._compare(
             'surgeo_input.csv',
             'surgeo',
             'surgeo_output.csv',
@@ -65,7 +70,7 @@ class TestSurgeoCLI(unittest.TestCase):
 
     def test_sur_cli(self):
         """Test surname model functionality of CLI"""
-        self.compare(
+        self._compare(
             'surname_input.csv',
             'sur',
             'surname_output.csv',
@@ -73,7 +78,7 @@ class TestSurgeoCLI(unittest.TestCase):
 
     def test_geo_cli(self):
         """Test geocode model functionality of CLI"""
-        self.compare(
+        self._compare(
             'geocode_input.csv',
             'geo',
             'geocode_output.csv',
@@ -95,13 +100,30 @@ class TestSurgeoCLI(unittest.TestCase):
         df_generated = pd.read_excel(self._EXCEL_OUTPUT_PATH)
         # Read the true information
         df_true = pd.read_excel(self._DATA_FOLDER / 'surgeo_output.xlsx')
-        # Compare values
-        self.assertTrue(
-            df_generated.equals(df_true)
-        )        
+        self._is_close_enough(df_generated, df_true)
 
     def test_malformed(self):
-        pass
+        """Test arguments to specify column names"""
+        # Generate input name based on input file
+        input_path = str(self._DATA_FOLDER / 'surgeo_input_misnamed.csv')
+        # Run a process that writes to CSV output
+        subprocess.run([
+            sys.executable,
+            self._CLI_SCRIPT,
+            input_path,
+            self._CSV_OUTPUT_PATH,
+            'surgeo',
+            '--zcta_column',
+            'info_zip',
+            '--surname_column',
+            'info_name',
+        ])
+        # Read the newly generated information
+        df_generated = pd.read_csv(self._CSV_OUTPUT_PATH)
+        # Read the true information
+        df_true = pd.read_csv(self._DATA_FOLDER / 'surgeo_output.csv')
+        self._is_close_enough(df_generated, df_true)
+
 
 if __name__ == '__main__':
     unittest.main()
